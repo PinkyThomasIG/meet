e4e4b95b9ea7ade81bf8e2b923bd039c5a4e99d5
PinkyThomasIG <pinkymukalal@gmail.com>
2025-04-07 11:21:11 +0200

    New project on atatus has been created

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
 import * as atatus from "atatus-spa";
-atatus.config("4dfc8db270b0428e906a306f8b8df490").install();
+atatus.config("9d866daa1c834bb28981071252c2620a").install();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

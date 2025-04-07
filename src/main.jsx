ef5188429fcd20b645efc073f5f0466fe5bb7672
PinkyThomasIG <pinkymukalal@gmail.com>
2025-04-07 10:33:09 +0200

    atatus installed and config code and and test error added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as atatus from "atatus-spa";
+atatus.config("4dfc8db270b0428e906a306f8b8df490").install();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+atatus.notify(new Error("Test Atatus Setup"));

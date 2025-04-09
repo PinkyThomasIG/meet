5956296c4f4f47fe9ee64f7a0d92d6bf41bd9e1d
PinkyThomasIG <pinkymukalal@gmail.com>
2025-04-09 11:07:35 +0200

    service worker files has been added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 
 import * as atatus from "atatus-spa";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 atatus.config("9d866daa1c834bb28981071252c2620a").install();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -12,4 +14,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-atatus.notify(new Error("Test Atatus Setup"));
+// atatus.notify(new Error("Test Atatus Setup"));
+serviceWorkerRegistration.register();
